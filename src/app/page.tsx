5cc5340266fd1fc254c65543a0b4d03719157414
Aryan Prince <aryanprincesanty@icloud.com>
2024-06-11 17:02:57 +0300

    ➕ Add and setup Tailwind CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main>
-      <h1>Hello World</h1>
+      <h1 className="text-3xl text-red-500 font-bold underline">
+        Hello world!
+      </h1>
     </main>
   )
 }
